c612e6ad29169a8eafef695e57790f7ff3aa9a0e
feelitbreak <88198704+feelitbreak@users.noreply.github.com>
2022-02-15 17:26:40 +0300

    change1

--- lab1/ConsoleApplication1.cpp
+++ lab1/ConsoleApplication1.cpp
@@ -1,5 +1,3 @@
-// Пример создания потока функцией CreateThread
-
 #include <windows.h>
 #include <iostream>
 #include <ctime>
@@ -9,26 +7,37 @@ volatile int n;
 
 struct Mass {
 	int n;
-	int *A;
-	Mass(int n, int* A) {
+	int* A;
+	long end;
+	Mass(int n, int* A, long end) {
 		this->n = n;
-		this->A = A;
+		this->A = new int[n];
+		for (int i = 0; i < n; i++) {
+			this->A[i] = A[i];
+		}
+		this->end = end;
 	}
 };
 
-DWORD WINAPI worker(LPVOID iNum)
+DWORD WINAPI worker(LPVOID par)
 {
-	Mass *m = (Mass*) iNum;
+	Mass* m = (Mass*)par;
 	int n = m->n;
 	int* A = m->A;
+	long end = m->end;
+	int count = 0;
+	cout << "Массив:" << endl;
 	for (int i = 0; i < n; i++)
 	{
-
 		cout << A[i] << endl;
+		if (A[i]) {
+			count++;
+		}
 		// приостанавливаем поток на 12 миллисекунд
 		Sleep(12);
 	}
-
+	Sleep(end);
+	cout << "Количество ненулевых элементов = " << count << endl;
 	return 0;
 }
 
@@ -38,7 +47,7 @@ int main()
 	HANDLE	hThread;
 	DWORD	IDThread;
 	int n;
-	int *A;
+	int* A;
 
 
 	setlocale(LC_ALL, "rus");
@@ -48,15 +57,27 @@ int main()
 	A = new int[n];
 	srand((int)time(NULL));
 	for (int i = 0; i < n; i++) {
-		A[i] = (int)rand();
+		A[i] = (int)rand() % 20 - 10;
 	}
-	Mass* mass = new Mass(n, A);
+	/*cout << "Введите элементы массива." << endl;
+	for (int i = 0; i < n; i++)
+	cin >> A[i];*/
 
+	long start, end;
+	cout << "Введите время для остановки потока." << endl;
+	cin >> end;
+	cout << "Введите время для запуска потока." << endl;
+	cin >> start;
+
+	Mass* mass = new Mass(n, A, end);
 	hThread = CreateThread(NULL, 0, worker, (void*)mass, 0, &IDThread);
 	if (hThread == NULL)
 		return GetLastError();
 
-	
+	SuspendThread(hThread);
+	Sleep(start);
+	ResumeThread(hThread);
+
 
 	delete[] A;
 	// ждем пока поток Add закончит работу
