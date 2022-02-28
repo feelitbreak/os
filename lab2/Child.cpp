dcd4b400a3416e2c5c5a6aaa7311ad2d219f86fa
feelitbreak <88198704+feelitbreak@users.noreply.github.com>
2022-02-28 09:21:27 +0300

    lab2 2

--- lab2/Child.cpp
+++ lab2/Child.cpp
@@ -4,13 +4,17 @@ int main(int argc, char* argv[])
 {
 	int i;
 	int n = (int)argv[1];
+	char* A = new char[n];
+	for (int i = 2; i < n + 2; i++) {
+		A[i] = (char)argv[i];
+	}
 	_cputs("I am created.");
 
 	_cputs("\nMy name is: ");
 	_cputs(argv[0]);
 
-	for (i = 1; i < argc; i++)
-		_cprintf("\n My %d parameter =  %s", i, argv[i]);
+
+	delete[] A;
 
 	_cputs("\nPress any key to finish.\n");
 	_getch();
--- lab2/Parent.cpp
+++ lab2/Parent.cpp
@@ -1,19 +1,40 @@
-// Пример консольного процесса, который создает другое
-// консольное приложение с новой консолью и ждет завершения
-// работы этого приложения.
-
+#define _CRT_SECURE_NO_WARNINGS
 #include <windows.h>
+#include <iostream>
+#include <string>
+#include <ctime>
 #include <conio.h>
+using namespace std;
 
 int main()
 {
-	char lpszAppName[] = "E:\\ConsoleApplication2\\Debug\\Child.exe";
+	setlocale(LC_ALL, "rus");
+
+	string a = "Child.exe";
+	string b;
+	cout << "Введите размер массива." << endl;
+	int n;
+	cin >> n;
+	a += " ";
+	a += n;
+	cout << "Введите элементы массива." << endl;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> b;
+		a += " ";
+		a += b;
+	}
+	n = a.length();
+	char* lpszCommandLine = new char[n + 1];
+	strcpy(lpszCommandLine, a.c_str());
 	STARTUPINFO si;
 	PROCESS_INFORMATION piApp;
 	ZeroMemory(&si, sizeof(STARTUPINFO));
 	si.cb = sizeof(STARTUPINFO);
+	si.dwYCountChars = 50;
+	si.dwFlags = STARTF_USECOUNTCHARS;
 
-	if (!CreateProcess(lpszAppName, NULL, NULL, NULL, FALSE,
+	if (!CreateProcess(NULL, lpszCommandLine, NULL, NULL, FALSE,
 		CREATE_NEW_CONSOLE, NULL, NULL, &si, &piApp))
 	{
 		_cputs("The new process is not created.\n");
@@ -23,6 +44,7 @@ int main()
 		return 0;
 	}
 	_cputs("The new process is created.\n");
+
 	// ждем завершения созданного прцесса
 	WaitForSingleObject(piApp.hProcess, INFINITE);
 	// закрываем дескрипторы этого процесса в текущем процессе
