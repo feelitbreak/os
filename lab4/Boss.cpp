c26fb7e7321f7d73621035eb3c533e4ffaeb5106
feelitbreak <88198704+feelitbreak@users.noreply.github.com>
2022-04-04 09:38:27 +0300

    lab4 2

--- lab4/Boss.cpp
+++ lab4/Boss.cpp
@@ -1,20 +1,121 @@
-// ConsoleApplication4.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+//Кендысь Алексей, 2 курс, 9 группа. Лабораторная №4
 
+#include <windows.h>
 #include <iostream>
+
+using namespace std;
+
+
+HANDLE EventA, EventB, EventEndChild, EventEndParent;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	EventA = CreateEvent(NULL, FALSE, FALSE, NULL);
+	if (EventA == NULL)
+		return GetLastError();
+	EventB = CreateEvent(NULL, FALSE, FALSE, NULL);
+	if (EventB == NULL)
+		return GetLastError();
+	EventEndChild = CreateEvent(NULL, FALSE, FALSE, NULL);
+	if (EventEndChild == NULL)
+		return GetLastError();
+	EventEndParent = CreateEvent(NULL, FALSE, FALSE, NULL);
+	if (EventEndParent == NULL)
+		return GetLastError();
+
+	HANDLE hSemaphore;
+	hSemaphore = CreateSemaphore(NULL, 0, 10, "ParentSemaphore");
+	if (hSemaphore == NULL)
+		return GetLastError();
+
+	HANDLE	hMutex;
+	hMutex = CreateMutex(NULL, FALSE, "ChildMutex");
+	if (hMutex == NULL)
+	{
+		cout << "Create mutex failed." << endl;
+		cout << "Press any key to exit." << endl;
+		cin.get();
+
+		return GetLastError();
+	}
+
+	int nPr1, nPr2;
+	cout << "Input the amount of Parent processes." << endl;
+	cin >> nPr1;
+	cout << "Input the amount of Child processes." << endl;
+	cin >> nPr2;
+
+	int nMes1, nMes2;
+	cout << "Input the amount of Parent messages." << endl;
+	cin >> nMes1;
+	cout << "Input the amount of Child messages." << endl;
+	cin >> nMes2;
+
+	char	lpszAppNameParent[] = "Parent.exe";
+	char	lpszAppNameChild[] = "Child.exe";
+	STARTUPINFO	siChild;
+	STARTUPINFO	siParent;
+	PROCESS_INFORMATION	piChild;
+	PROCESS_INFORMATION	piParent;
+
+	ZeroMemory(&siChild, sizeof(STARTUPINFO));
+	siChild.cb = sizeof(STARTUPINFO);
+
+	ZeroMemory(&siParent, sizeof(STARTUPINFO));
+	siParent.cb = sizeof(STARTUPINFO);
+
+	if (!CreateProcess(lpszAppNameChild, NULL, NULL, NULL, FALSE,
+		NULL, NULL, NULL, &siChild, &piChild))
+	{
+		cout << "The new process Child is not created." << endl;
+		cout << "Press any key to exit." << endl;
+		cin.get();
+
+		return GetLastError();
+	}
+
+	if (!CreateProcess(lpszAppNameParent, NULL, NULL, NULL, FALSE,
+		NULL, NULL, NULL, &siParent, &piParent))
+	{
+		cout << "The new process Parent is not created." << endl;
+		cout << "Press any key to exit." << endl;
+		cin.get();
+
+		return GetLastError();
+	}
+
+	// выводим на экран строки
+	for (int j = 0; j < 10; j++)
+	{
+		// захватываем мьютекс
+		WaitForSingleObject(hMutex, INFINITE);
+		for (int i = 0; i < 10; i++)
+		{
+			cout << j << ' ';
+			cout.flush();
+			Sleep(10);
+		}
+		cout << endl;
+		// освобождаем мьютекс
+		ReleaseMutex(hMutex);
+	}
+
+	CloseHandle(hSemaphore);
+	CloseHandle(hMutex);
+
+	WaitForSingleObject(piChild.hProcess, INFINITE);
+	WaitForSingleObject(piParent.hProcess, INFINITE);
+
+	CloseHandle(piChild.hThread);
+	CloseHandle(piChild.hProcess);
+
+	CloseHandle(piParent.hThread);
+	CloseHandle(piParent.hProcess);
+
+	CloseHandle(EventA);
+	CloseHandle(EventB);
+	CloseHandle(EventEndChild);
+	CloseHandle(EventEndParent);
+
+	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
--- lab4/Child/Child.cpp
+++ lab4/Child/Child.cpp
@@ -1,20 +1,40 @@
-// Child.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
+#include <windows.h>
 #include <iostream>
+
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	HANDLE	hMutex;
+	int		i, j;
+
+	// открываем мьютекс
+	hMutex = OpenMutex(SYNCHRONIZE, FALSE, "ChildMutex");
+	if (hMutex == NULL)
+	{
+		cout << "Open mutex failed." << endl;
+		cout << "Press any key to exit." << endl;
+		cin.get();
+
+		return GetLastError();
+	}
+
+	for (j = 10; j < 20; j++)
+	{
+		// захватываем мьютекс
+		WaitForSingleObject(hMutex, INFINITE);
+		for (i = 0; i < 10; i++)
+		{
+			cout << j << ' ';
+			cout.flush();
+			Sleep(5);
+		}
+		cout << endl;
+		// освобождаем мьютекс
+		ReleaseMutex(hMutex);
+	}
+	// закрываем дескриптор объекта
+	CloseHandle(hMutex);
+
+	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
