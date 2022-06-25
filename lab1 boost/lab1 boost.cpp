b65e3e835b8dca7096249484912f5b850bddb852
feelitbreak <88198704+feelitbreak@users.noreply.github.com>
2022-06-25 19:20:12 +0300

    boost 1

---
+++ lab1 boost/lab1 boost.cpp
@@ -0,0 +1,65 @@
+﻿#include <boost/thread.hpp> 
+#include <iostream> 
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+static int const rangeOfArray = 20;
+static int const numOfNegativeElem = 10;
+
+void wait(int seconds)
+{
+    boost::this_thread::sleep(boost::posix_time::seconds(seconds));
+}
+
+void thread(int time, int n, int* mass)
+{
+    int count = 0;
+    int halfWayThrough = n / 2;
+
+    cout << "Array:" << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cout << mass[i] << " ";
+        if (mass[i]) {
+            count++;
+        }
+        if (i == halfWayThrough)
+        {
+            wait(time);
+        }
+    }
+    cout << endl;
+
+    cout << "The number of non-zero elements = " << count << endl;
+}
+
+int main()
+{
+    int n;
+    int* A;
+
+    cout << "Input the size of the array." << endl;
+    cin >> n;
+
+    A = new int[n];
+    srand((int)time(NULL));
+    for (int i = 0; i < n; i++) {
+        A[i] = (int)rand() % rangeOfArray - numOfNegativeElem;
+    }
+
+    int start, end;
+    cout << "Input the time for starting the thread." << endl;
+    cin >> start;
+    cout << "Input the time for suspending the thread." << endl;
+    cin >> end;
+
+    wait(start);
+
+    boost::thread t(thread, end, n, A);
+    t.join();
+
+    delete[] A;
+    return 0;
+}
